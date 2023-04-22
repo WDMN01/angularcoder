65cc2cd31ffffd3e2471bb6f2303ae036837d772
WDMN01 <wdavidmartinezn@hotmail.com>
2023-04-21 21:04:02 -0500

    segundo desafio pipes, interpolacion y directivas

--- project/src/app/students/students.component.ts
+++ project/src/app/students/students.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-students',
   templateUrl: './students.component.html',
   styleUrls: ['./students.component.css']
 })
-export class StudentsComponent {
+
+export class StudentsComponent implements OnInit{
+  listStyles = { 'background-color': 'lightblue', 'border': '1px solid black' };
+  public estudiantes =[ 'Isabela' , 'Juan', 'Carlos', 'Efrain', 'Jorge'];
+  ngOnInit(): void {
+    
+  }
 
 }
